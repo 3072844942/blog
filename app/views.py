508c4eb2f6f55065994be07eea2c174b2092783d
3072844942 <3072844942@qq.com>
2022-01-30 10:09:41 +0800

    modify rotate pictures

--- app/views.py
+++ app/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    img_list = [
+        "/static/image/header/background1.jpg",
+        "/static/image/header/background2.jpg",
+    ]
+    return render(request, 'index.html', { "img_list" : img_list })
