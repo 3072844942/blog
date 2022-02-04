c0a5fc18b6a521070f23837a9c76da71676a19f4
3072844942 <3072844942@qq.com>
2022-02-04 17:50:58 +0800

    write footer

--- app/templatetags/my_tag.py
+++ app/templatetags/my_tag.py
@@ -3,6 +3,14 @@ from django import template
 register = template.Library()
 
 
-@register.filter
-def add1(item):
-    return int(item) + 1
+# @register.filter
+# def add1(item):
+#     return int(item) + 1
+
+@register.inclusion_tag('my_tag/headers.html')
+def banner(menu_name):
+    img_list = [
+        "/static/image/header/background1.jpg",
+        "/static/image/header/background2.jpg",
+    ]
+    return { "img_list" : img_list }
--- app/views.py
+++ app/views.py
@@ -3,8 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    img_list = [
-        "/static/image/header/background1.jpg",
-        "/static/image/header/background2.jpg",
-    ]
-    return render(request, 'index.html', { "img_list" : img_list })
+    return render(request, 'index.html')
