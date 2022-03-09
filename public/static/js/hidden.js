0e12125a298b765ffd2744e7c2506b29607bb2b2
3072844942 <3072844942@qq.com>
2022-03-09 12:50:43 +0800

    修正自动登录

---
+++ public/static/js/hidden.js
@@ -0,0 +1,10 @@
+var normalTitle = document.title
+window.onfocus = function () {
+    document.title = "(/≥▽≤)咦! 你又回来啦";
+    setTimeout(function (){
+        document.title = normalTitle;
+    }, 1500);
+};
+window.onblur = function () {
+    document.title = "等等, 你别走啊!!!";
+}
